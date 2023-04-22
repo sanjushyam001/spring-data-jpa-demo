0ea9b8a74ae5ddc823ce66e6b0409b894fc4c871
sanjushyam001 <kumarsanjuashokit@gmail.com>
2023-04-22 16:25:23 +0530

    my second commit here

--- src/main/java/in/nareshit/SpringBoot2DataRestApplication.java
+++ src/main/java/in/nareshit/SpringBoot2DataRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBoot2DataRestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Testing!");
 		SpringApplication.run(SpringBoot2DataRestApplication.class, args);
 	}
 
