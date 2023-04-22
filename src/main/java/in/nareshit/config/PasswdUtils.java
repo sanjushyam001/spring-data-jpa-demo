d314e57d1ccbd0c2f2172c3ada532c05d6f43907
sanjushyam001 <kumarsanjuashokit@gmail.com>
2023-04-22 17:37:40 +0530

    fourth commit

--- src/main/java/in/nareshit/config/PasswdUtils.java
+++ src/main/java/in/nareshit/config/PasswdUtils.java
@@ -3,12 +3,12 @@ package in.nareshit.config;
 public class PasswdUtils {
 	
 	public void encode() {
-	
-		int i=0;
+		
 	}
 	public void decode() {
 		int x=10;
 		int y=20;
+		int z=40;
 				
 	}
 }
