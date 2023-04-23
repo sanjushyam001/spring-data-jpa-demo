cf2bd7deee6e43d8d5a24bdc1cf05abc11f1bb9d
sanjushyam001 <kumarsanjuashokit@gmail.com>
2023-04-23 18:05:30 +0530

    change passwdutils class ..

--- src/main/java/in/nareshit/config/PasswdUtils.java
+++ src/main/java/in/nareshit/config/PasswdUtils.java
@@ -3,25 +3,12 @@ package in.nareshit.config;
 public class PasswdUtils {
 	
 	public void encode() {
-		int i=0;
-		int j=1;
-
-		String nameString="Ashok IT";
-
-		int a=0;
-		int b=0;
-		//bug fixed 104
-		System.out.println("Changes are made !");
+		
+		System.out.println("Changes are made in encode() for feature-01 !");
 			
 	}
 	public void decode() {
-		int x=10;
-		int y=20;
-		int z=40;
-		
-		double d1=10.0;
-		double d2=15.0;
-		double d4=d1+d2;
+		System.out.println("Changes are made in decode() for feature-01 !");
 				
 	}
 }
